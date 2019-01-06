4469d6d14a9d7a972b0c6ef0ef64e1899fbc8796
OUARRICH <warichsaid@gmail.com>
2019-01-06 18:07:03 +0000

    comment mongo db

--- server.js
+++ server.js
@@ -17,28 +17,28 @@ var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
 //Test running app without connecting to database
-// app.listen(PORT, () => console.log(`Argana app is now Listening on ${PORT}`));
+app.listen(PORT, () => console.log(`Argana app is now Listening on ${PORT}`));
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 // var db;
 
 // // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/argana", function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
+// mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/argana", function (err, client) {
+//   if (err) {
+//     console.log(err);
+//     process.exit(1);
+//   }
 
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
+//   // Save database object from the callback for reuse.
+//   db = client.db();
+//   console.log("Database connection ready");
 
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-});
+//   // Initialize the app.
+//   var server = app.listen(process.env.PORT || 8080, function () {
+//     var port = server.address().port;
+//     console.log("App now running on port", port);
+//   });
+// });
 
 // // CONTACTS API ROUTES BELOW
 
@@ -52,15 +52,15 @@ function handleError(res, reason, message, code) {
 //  *    GET: finds all contacts
 //  *    POST: creates a new contact
 //  */
-app.get("/api/products", function(req, res) {
-  db.collection(PRODUCTS_COLLECTION).find({}, {name:1, price:1, inStock:1, onOrder:1, _id:0}).toArray(function(err, docs) {
-    if (err) {
-      handleError(res, err.message, "Failed to get products.");
-    } else {
-      res.status(200).json(docs);
-    }
-  });
-});
+// app.get("/api/products", function(req, res) {
+//   db.collection(PRODUCTS_COLLECTION).find({}).toArray(function(err, docs) {
+//     if (err) {
+//       handleError(res, err.message, "Failed to get products.");
+//     } else {
+//       res.status(200).json(docs);
+//     }
+//   });
+// });
 
 // app.post("/api/contacts", function(req, res) {
 //   var newContact = req.body;
